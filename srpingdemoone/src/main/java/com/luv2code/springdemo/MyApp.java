3cff3065a9f7ae4bee1e371076ab5f465b9fdafb
Etibarli Parvin <etibarliparvin@gmail.com>
2021-06-29 16:59:48 +0400

    my first spring application

---
+++ srpingdemoone/src/main/java/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+    public static void main(String[] args) {
+         Coach baseballCoach = new BaseballCoach();
+        System.out.println(baseballCoach.getDailyWorkout());
+    }
+}
