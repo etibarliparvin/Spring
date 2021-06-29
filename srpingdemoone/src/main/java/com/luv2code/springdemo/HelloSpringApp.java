cde83db630884d196a776fa88d38aa461565c3b1
Etibarli Parvin <etibarliparvin@gmail.com>
2021-06-29 19:59:21 +0400

    some changes

--- srpingdemoone/src/main/java/com/luv2code/springdemo/HelloSpringApp.java
+++ srpingdemoone/src/main/java/com/luv2code/springdemo/HelloSpringApp.java
@@ -10,6 +10,10 @@ public class HelloSpringApp {
         Coach coach = context.getBean("myCoach", Coach.class);
         // call methods on the bean
         System.out.println(coach.getDailyWorkout());
+
+        // let's call our new method for fortunes
+        System.out.println(coach.getDailyFortune());
+
         // close the context
         context.close();
     }
--- srpingdemoone/src/main/java/com/luv2code/springdemo/MyApp.java
+++ srpingdemoone/src/main/java/com/luv2code/springdemo/MyApp.java
@@ -2,7 +2,6 @@ package com.luv2code.springdemo;
 
 public class MyApp {
     public static void main(String[] args) {
-         Coach baseballCoach = new BaseballCoach();
-        System.out.println(baseballCoach.getDailyWorkout());
+
     }
 }
